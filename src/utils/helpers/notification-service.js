cf3c19b3b763483e91afdc55c3b0729327143a15
Denmark Amano Daya <dadaya@sbfinance.com.ph>
2024-06-19 08:01:01 +0800

    cleanup 2

--- src/utils/helpers/notification-service.js
+++
@@ -1,4 +0,0 @@
-const { UdNotificationServiceAxiosConfig } = require('../../common/axios-config');
-const { post } = require('./axios')(UdNotificationServiceAxiosConfig);
-
-module.exports.sendTransactionalNotification = async (payload) => post('/notification/transactional', payload);
