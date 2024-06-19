4775dac8f9d9eeae5a799b9c4bb875e4ed44cf21
Denmark Amano Daya <dadaya@sbfinance.com.ph>
2024-06-19 14:57:47 +0800

    updated line-break style

--- src/common/logger.js
+++ src/common/logger.js
@@ -14,7 +14,7 @@ const logger = winston.createLogger({
     enumerateErrorFormat(),
     winston.format.colorize(),
     winston.format.splat(),
-    winston.format.printf(({ level, message }) => `${level} : ${message}`)
+    winston.format.printf(({ level, message }) => `${level} : ${message}`),
   ),
   transports: [
     new winston.transports.Console({
