5f14faf305b45d60ece2e87f5ab4c26f0f0a87bc
Denmark Daya <denmarkamanodaya.fs360@gmail.com>
2024-12-16 06:34:08 +0800

    Added Health API

---
+++ src/handlers/health/v1.js
@@ -0,0 +1,13 @@
+const { defaultMiddleware: middleware } = require('../../middlewares/middy');
+const { httpResponseCodes } = require('../../common/response-codes');
+const logger = require('../../common/logger');
+const httpStatus = require('http-status');
+
+module.exports.handler = middleware(async ({ headers, body }) => {
+  logger.debug(JSON.stringify({ headers, body }));
+
+  return {
+    status: httpStatus.OK,
+    code: httpResponseCodes.VALID.value
+  };
+});
