f7bb198380acbc9f3b3b65abd8c3c63bf55e0856
Denmark Daya <denmarkamanodaya.fs360@gmail.com>
2024-12-26 11:45:09 +0800

    fix: Fixed script run command

--- cd_script/set-variables.sh
+++ cd_script/set-variables.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#Setting up ENV variables
+echo "setting up env variables"
+
+deployerRoleArn='arn:aws:iam::153326081486:role/xplor-deployer'
+deploymentBucket='xplor-dev-iam-deployment-bucket'
+apiGatewayId='2bsvo6b74l'
+rootResourceId='n8gag72sl7'
+
+export DEPLOYER_ROLE=$deployerRoleArn
+export DEPLOYMENT_BUCKET=$deploymentBucket
+export API_GATEWAY_ID=$apiGatewayId
+export ROOT_RESOURCE_ID=$rootResourceId
+
+echo $ROOT_RESOURCE_ID
+
 # export ACTIVE_BRANCH=$BITBUCKET_BRANCH
 
 # deployerRoleArn=''
@@ -73,17 +88,11 @@
 # export SLS_STAGE=$slsStage
 # export SLS_REGION=$slsRegion
 
-echo "setting up env variables"
-
 # export VPC_SETTINGS=$vpcSettings
-export DEPLOYER_ROLE='arn:aws:iam::153326081486:role/xplor-deployer'
 # export DEPLOYMENT_ROLE=$deploymentRoleArn
-export DEPLOYMENT_BUCKET='xplor-dev-iam-deployment-bucket'
 # export SUBNET_ID_A=$subnetIdA
 # export SUBNET_ID_B=$subnetIdB
 # export SECURITY_GROUP_ID=$securityGroupId
 # export EXECUTION_ROLE=$executionRoleArn
-export API_GATEWAY_ID='2bsvo6b74l'
-export ROOT_RESOURCE_ID='n8gag72sl7'
 # export NODE_ENV=$environmentName
 
